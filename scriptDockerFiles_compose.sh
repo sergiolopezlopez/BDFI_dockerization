4bbee563fd66312c324c1291bfce4b58c8f941ac
Sergio López <s.llopez@alumnos.upm.es>
2020-11-30 17:15:59 +0100

    docker en git

---
+++ scriptDockerFiles_compose.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd kafka_docker
+docker build -t sllopez/kafka_p1 .
+
+cd ../server_docker
+docker build -t sllopez/server_p1 .
+
+cd ../spark_docker
+docker build -t sllopez/spark_p1 .
+
+# cd ../mongo_docker
+# docker build -t sllopez/mongo_p1 .
+
+cd ..
+docker-compose up
+
